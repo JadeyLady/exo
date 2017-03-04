a79289ee4c478693a81617e6912f0cdc9b0a9ed3
Jade Rodriguez <jadesrdz@gmail.com>
2017-03-04 21:49:37 +0000

    band trash the sequel

--- index.php
+++ index.php
@@ -1,29 +1,62 @@
-<!-- This is the basic webpage-->
-<!-- We call it a hypertext markup language document -->
 <!DOCTYPE html>
-<!-- set default language -->
 <html lang="en">
-<!-- header: stuff we reference will go here (Libraries, Stylesheet stuff) -->
 <head> 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1"> 
-
-  <!-- These scripts give us access to the bootstrap font end framework, which makes styling pretty easy!  -->
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-</head> 
-<!-- body: where we will put the stuff we can see! -->
+  <!-- Add the Plotly library -->
+  <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+
+  <!-- include the google font for the styles -->
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+
+
+
+
+  <!-- adding a bit of style here using css in the header -->
+  <style>
+  /*changing the background color and the font of the navbar, and removing the border*/
+  .navbar-default{
+      background-color: #fff;
+      border: none;
+      font-family:'Playfair Display', serif; 
+    }
+  /*padding the  section so we get some room between the navbar and the first item */
+    #section{
+      padding-top: 100px;
+    }
+    /*padding the bottom so our copywrite is positioned nicely*/
+    body {
+     padding-bottom: 50px; 
+    }
+    /*changing teh  font and increasing the size of the font*/
+    footer{
+      font-family:'Playfair Display', serif; 
+      font-size: 24px;
+    }
+  </style>
+
+</head>
 <body>
-  <!-- let's add more! We'll put in some sections that we can fill with our examples!  -->
-    <h1>My Page</h1>
-    <div id = "section">
+  <!-- Let's add header and footer!  -->
+  <div id= "navbar"> 
+     <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <h1> My Page </h1>
+        </div>
+      </nav>
+   </div>   
+  <div id = "section">
   
     <div class= "container">
       <div class = "jumbotron">
-        <h2>First Chart Type</h2>
+        <h2>First Chart Type: Bar Graph with Plotly</h2>
         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-        
+         <div id="myDiv" style="width: 480px; height: 400px;">
+          <!-- Plotly chart will be drawn here -->
+        </div>
       </div>
     </div>  
     
@@ -54,6 +87,32 @@
     </div> 
 
   </div>
-<!-- End Charts -->
+<!-- Let's add header & Footer -->
+  <footer>
+      <div class="container">
+        <div id="section">
+          <div class="row">
+            <p class="copyright"> &copy 2017 Friendly Fulcrum </p>
+          </div>
+        </div>  
+      </div>
+  </footer>
       
 </body>
+<!-- We need to put the script in <script> </script> tags so the page knows it's Javascript, in this case! -->
+<script>
+// This is the Bar Chart
+// First, define what the data is by seeting data as an array of attributes setting the x & y  axis, setting the marker colors and defining the type of graph
+var data = [
+  {
+    x: ['Prior Experience', 'No Experience', 'Some Experience'],
+    y: [20, 14, 23],
+    marker: {
+      color: ['rgb(82,185,191)', 'rgb(234,100,156)', 'rgb(112,99,86)']
+    }, 
+    type: 'bar'
+  }
+];
+// Next, tell Plotly to make a new plot in the  div above called "My Div" using "data"
+Plotly.newPlot('myDiv', data);
+</script>
